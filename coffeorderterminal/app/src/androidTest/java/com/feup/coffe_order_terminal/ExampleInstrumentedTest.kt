e03fed74a19676581fc6145edc7a45e07e4f39f3
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-03-12 00:39:54 +0000

    terminal application init

---
+++ coffeorderterminal/app/src/androidTest/java/com/feup/coffe_order_terminal/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.feup.coffe_order_terminal
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.feup.coffe_order_terminal", appContext.packageName)
+    }
+}
