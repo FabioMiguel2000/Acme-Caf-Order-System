8ffa309bafeb3a6b983f01e82e77b21f56ea1d8a
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-16 17:58:38 +0000

    server/refactor: setup.js

--- coffee-order-api/utils/setup.js
+++ coffee-order-api/utils/setup.js
@@ -1,88 +1,66 @@
 const fs = require("fs");
 const path = require("path");
-const mongoose = require("mongoose");
 
 const dbconnection = require("../services/dbconnection");
 const { encryptPasswords } = require("./crypto/bcryptPassword");
 
-const productSeeders = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, "./seeders/productSeeders.json"),
-    "utf-8"
-  )
-);
-const userSeeders = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "./seeders/userSeeders.json"), "utf-8")
-);
+// Modularize file reading
+const readJSONFile = (fileName) =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, `./seeders/${fileName}.json`), "utf-8"));
 
-const orderSeeders = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "./seeders/orderSeeders.json"), "utf-8")
-);
+const productSeeders = readJSONFile("productSeeders");
+const userSeeders = readJSONFile("userSeeders");
+const orderSeeders = readJSONFile("orderSeeders");
 
 const Product = require("../models/product");
 const User = require("../models/user");
 const Order = require("../models/order");
 
 const createOrderByNifAndProductNames = async (order) => {
-  const user = await User.findOne({ nif: order.client });
-  let products = [];
-
-  for (let i = 0; i < order.products.length; i++) {
-    let product = await Product.findOne({ name: order.products[i].product_name });
-    products.push({_id: product, quantity: order.products[i].quantity});
-  }
-  const orderObj = new Order({
-    client: user._id,
-    products,
+  const user = await User.findOne({ nif: order.client }).exec();
+  const productPromises = order.products.map(async (p) => {
+    const product = await Product.findOne({ name: p.product_name }).exec();
+    return { _id: product._id, quantity: p.quantity };
   });
+  const products = await Promise.all(productPromises);
 
-  await orderObj.save();
-
-}
+  await new Order({ client: user._id, products }).save();
+};
 
 const importData = async () => {
   try {
     await dbconnection();
-
     const userSeedersEncrypted = await encryptPasswords(userSeeders);
 
-    await Product.deleteMany();
-    await User.deleteMany();
-    await Order.deleteMany();
-
-    await Product.insertMany(productSeeders);
-    await User.insertMany(userSeedersEncrypted);
-
-    for (const orderData of orderSeeders) {
-      await createOrderByNifAndProductNames(orderData);
-    }
+    await Promise.all([Product.deleteMany(), User.deleteMany(), Order.deleteMany()]);
+    await Promise.all([Product.insertMany(productSeeders), User.insertMany(userSeedersEncrypted)]);
+    await Promise.all(orderSeeders.map(createOrderByNifAndProductNames));
 
     console.log("Data has been seeded!");
-
-    process.exit();
+    gracefulExit();
   } catch (error) {
-    console.log("Failed to seed MongoDB:", error);
-    process.exit(1);
+    console.error("Failed to seed MongoDB:", error);
+    gracefulExit(1);
   }
 };
 
 const destroyData = async () => {
   try {
     await dbconnection();
-
-    await Product.deleteMany();
-    await User.deleteMany();
+    await Promise.all([Product.deleteMany(), User.deleteMany(), Order.deleteMany()]);
 
     console.log("Data has been destroyed!");
-
-    process.exit();
+    gracefulExit();
   } catch (error) {
-    console.log("Failed to destroy MongoDB:", error);
-    process.exit(1);
+    console.error("Failed to destroy MongoDB:", error);
+    gracefulExit(1);
   }
 };
 
-if (process.argv[2] == "-d") {
+const gracefulExit = (code = 0) => process.exit(code);
+
+// Process command line arguments
+if (process.argv[2] === "-d") {
   destroyData();
 } else {
   importData();
