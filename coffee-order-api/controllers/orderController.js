b4e9f3e7b262d9983d8aceacb583caab45bfc05d
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-17 19:49:28 +0000

    server/feat: added total, subtotal, promotionDiscount to calculation

--- coffee-order-api/controllers/orderController.js
+++ coffee-order-api/controllers/orderController.js
@@ -63,11 +63,28 @@ const getOrderByUser = async (req, res) => {
 };
 
 const getProductObjs = async (products) => {
-    const productPromises = products.map(async (p) => {
-        const product = await Product.findOne({ _id: p.product });
-        return { product: product._id, quantity: p.quantity };
-    });
-    return await Promise.all(productPromises);
+  const productPromises = products.map(async (p) => {
+    const product = await Product.findOne({ _id: p.product });
+    return { product: product._id, quantity: p.quantity };
+  });
+  return await Promise.all(productPromises);
+};
+
+const calculatePrices = (products, discountVoucher) => {
+  let subtotal = 0;
+  let total = 0;
+  products.forEach((p) => {
+    subtotal += p.product.price * p.quantity;
+  });
+  if (discountVoucher) {
+    total = subtotal * (1 - discountVoucher.discount);
+  } else {
+    total = subtotal;
+  }
+
+  const promotionDiscount = total - subtotal;
+
+  return { subtotal, promotionDiscount, total };
 };
 
 const createOrder = async (req, res) => {
@@ -75,27 +92,39 @@ const createOrder = async (req, res) => {
     const { client, products } = req.body;
 
     const clientExists = await User.findById({
-        _id: client
+      _id: client,
     });
 
     if (!clientExists) {
-        return res.status(404).json({
-            error: true,
-            success: false,
-            message: `Client with id: ${client} not found`
-        });
+      return res.status(404).json({
+        error: true,
+        success: false,
+        message: `Client with id: ${client} not found`,
+      });
     }
 
     const productObjs = await getProductObjs(products);
 
-    const newOrder = await new Order({ client: clientExists._id, products: productObjs }).save();
+    const newOrder = await new Order({
+      client: clientExists._id,
+      products: productObjs,
+    }).save();
+
+    const { subtotal, promotionDiscount, total } = calculatePrices(
+      newOrder.products,
+      clientExists.discountVoucher
+    );
+
+    let orderObject = newOrder.toObject();
+    orderObject.subtotal = subtotal;
+    orderObject.promotionDiscount = promotionDiscount;
+    orderObject.total = total;
 
     return res.status(201).json({
       success: true,
       message: `Order created with id: ${newOrder._id}`,
-      data: newOrder,
+      data: orderObject,
     });
-
   } catch (error) {
     return res.status(500).json({
       error: true,
