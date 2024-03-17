d7b93b13383224e8c60a654bd6350faabe9d0a3c
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-17 17:25:11 +0000

    server/feat: get orders by client

--- coffee-order-api/controllers/orderController.js
+++ coffee-order-api/controllers/orderController.js
@@ -9,7 +9,6 @@ const getAllOrders = async (req, res) => {
       data: orders,
     });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
       error: true,
       success: false,
@@ -19,29 +18,46 @@ const getAllOrders = async (req, res) => {
 };
 
 const getOrderByID = async (req, res) => {
-    try {   
-        const { id } = req.params;
-        const order = await Order.findById(id);
-        console.log(order)
-        if (!order) {
-            return res.status(404).json({
-                error: true,
-                success: false,
-                message: `Order with id: ${id} not found`
-            });
-        }
-        return res.status(200).json({
-            success: true,
-            message: `Retrieved order with id: ${id}`,
-            data: order
-        });
-    } catch (error) {
-        return res.status(500).json({
-            error: true,
-            success: false,
-            message: "Failed to retrieve order"
-        });
+  try {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+    if (!order) {
+      return res.status(404).json({
+        error: true,
+        success: false,
+        message: `Order with id: ${id} not found`,
+      });
     }
+    return res.status(200).json({
+      success: true,
+      message: `Retrieved order with id: ${id}`,
+      data: order,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      success: false,
+      message: "Failed to retrieve order",
+    });
+  }
 };
 
-module.exports = { getAllOrders, getOrderByID };
+const getOrderByUser = async (req, res) => {
+  try {
+    const { client } = req.query;
+    const orders = await Order.find({ client });
+    return res.status(200).json({
+      success: true,
+      message: `Retrieved ${orders.length} orders from client: ${client}`,
+      data: orders,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      success: false,
+      message: "Failed to retrieve orders",
+    });
+  }
+};
+
+module.exports = { getAllOrders, getOrderByID, getOrderByUser };
--- coffee-order-api/routers/orderRoutes.js
+++ coffee-order-api/routers/orderRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 
 router.get("/orders", orderController.getAllOrders);
+router.get("/orders/client", orderController.getOrderByUser);
 router.get("/orders/:id", orderController.getOrderByID);
 
 module.exports = router;
