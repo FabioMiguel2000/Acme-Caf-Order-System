f83ef0318ad0403c67fdcdae7156df1d49f03ceb
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-03-19 02:05:30 +0000

    login request

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/LoginActivity.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/LoginActivity.kt
@@ -1,24 +1,33 @@
 package com.feup.coffee_order_application
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.feup.coffee_order_application.services.HttpHandlerClass
+import kotlin.concurrent.thread
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
         val imageView: ImageView = findViewById(R.id.logo)
         imageView.setBackgroundResource(R.drawable.logoshop)
-
-        //val http_hand =  HttpHandlerClass.getInstance()
-        //var url = http_hand._baseUrl
-        //http_hand.testApiConnection(http_hand._baseUrl)
-        var teste = "Ola"
+        var username = findViewById<EditText>(R.id.email_value)
+        var password = findViewById<EditText>(R.id.password_value)
+        val loginBtn: Button = findViewById(R.id.btn_login)
+        val http_handler =  HttpHandlerClass.getInstance()
+        loginBtn?.setOnClickListener{(
+            //Toast.makeText(this, username.toString() + " - "+password.toString(), Toast.LENGTH_LONG)
+            thread {
+                var response = http_handler.login(this, http_handler._baseUrl, username.text.toString(), password.text.toString())
+            }
+        )}
     }
 }
--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/HttpHandlerClass.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/HttpHandlerClass.kt
@@ -1,6 +1,12 @@
 package com.feup.coffee_order_application.services
-
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -12,18 +18,62 @@ class HttpHandlerClass private  constructor(baseUrl : String ){
     fun testApiConnection(address: String){
         var url: URL? = null
         var urlConnection: HttpURLConnection? = null
+
         try {
             url = URL(address)
-            urlConnection = (url.openConnection() as HttpURLConnection?)?.apply {
+            urlConnection = (url.openConnection() as HttpURLConnection).apply {
                 doInput = true
                 setRequestProperty("Content-Type", "application/json")
                 useCaches = false
-                connectTimeout = 500
+                requestMethod = "GET"
+                connectTimeout = 5000
 
                 if(responseCode == 200){
-                    Log.e("error", "OK")
+                    val resonse = readStream(inputStream)
+                    Log.e("response-message", "OK")
                 } else {
-                    Log.e("error", "NOK")
+                    Log.e("response-message", "NOK")
+                }
+            }
+        } catch (e: Exception){
+            Log.e("response-message", e.toString())
+            throw e
+        } finally {
+            if(urlConnection != null)
+                urlConnection.disconnect()
+        }
+    }
+
+    fun login(context: Context, baseUrl: String, username: String, password: String): Boolean{
+        _baseUrl = baseUrl + "api/auth/login"
+        var url = URL(_baseUrl)
+        var credentials: HashMap<String, String> = HashMap<String, String>()
+        credentials.put("username", username)
+        credentials.put("password", password)
+        var params = credentials.toString()
+
+        var urlConnection: HttpURLConnection ? = null
+        try {
+            urlConnection = (url.openConnection() as HttpURLConnection).apply {
+                doInput = true
+                doOutput = true
+                requestMethod = "POST"
+                setRequestProperty("Content-Type", "application/json")
+                useCaches = false
+                connectTimeout = 5000
+                with(outputStream) {
+                    write(params.toByteArray())
+                    flush()
+                    close()
+                }
+
+                if(responseCode == 200) {
+                    val response = readStream(inputStream)
+                    return true
+                } else{
+                    Log.e("message", "NOK")
+                    val response = readStream(inputStream)
+                    return false
                 }
             }
         } catch (e: Exception){
@@ -33,16 +83,30 @@ class HttpHandlerClass private  constructor(baseUrl : String ){
                 urlConnection.disconnect()
         }
     }
+
+    fun readStream(input: InputStream): String {
+        var reader: BufferedReader? = null
+        var line: String?
+        var response = StringBuilder()
+        try {
+            reader = BufferedReader(InputStreamReader(input))
+            while (reader.readLine().also { line = it } != null)
+                response.append(line)
+
+        }
+        catch (e: IOException){
+            response.clear()
+            response.append("readStream: ${e.message}")
+        }
+        reader?.close()
+        return response.toString()
+    }
     companion object {
         @Volatile private var instance: HttpHandlerClass? = null
-        //http://192.168.1.97:3000/
-        //https://myfakeapi.com/api/cars/1
         fun getInstance() = instance ?: synchronized(this) {
-            instance?:HttpHandlerClass("https://myfakeapi.com/api/cars/1").also {
+            instance?:HttpHandlerClass("http://192.168.1.97:3000/").also {
                 instance = it
             }
         }
-
-
     }
 }
