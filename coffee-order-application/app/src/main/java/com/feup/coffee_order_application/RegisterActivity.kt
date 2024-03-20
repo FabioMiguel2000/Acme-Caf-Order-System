45cabab5d327af8fc5b317ac7b64bd1ffb3f0050
Gabriel Gomes <gabrielgomes@Gabriels-MacBook-Pro-2.local>
2024-03-20 11:02:08 +0000

    toolbar in register activity added

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/RegisterActivity.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/RegisterActivity.kt
@@ -1,17 +1,24 @@
 package com.feup.coffee_order_application
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlin.concurrent.thread
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setTitle("Register")
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
         var name = findViewById<EditText>(R.id.name_value)
         var email = findViewById<EditText>(R.id.email_value)
@@ -20,5 +27,15 @@ class RegisterActivity : AppCompatActivity() {
         var passwordConfirmation = findViewById<EditText>(R.id.passwordconfirmation_value)
         val btnRegister: Button = findViewById(R.id.btn_register)
 
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
