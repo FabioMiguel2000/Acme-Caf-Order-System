73f400e2cba8948b569085678d0c89150364c5c8
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-20 15:53:50 +0000

    app/feat: displayed categories

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/Categories.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/Categories.kt
@@ -5,11 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.feup.coffee_order_application.adapters.CategoriesAdapter
+import com.feup.coffee_order_application.models.Category
+
 
 class Categories : Fragment() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categories = listOf<Category>(
+            Category("Hot Coffee", R.drawable.hot_coffee),
+            Category("Cold Coffee", R.drawable.ice_coffee),
+            Category("Cappucino", R.drawable.cappuccino)
+        )
+
+        val adapter = CategoriesAdapter(categories)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.rv_categories)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
     }
 
     override fun onCreateView(
@@ -20,6 +40,4 @@ class Categories : Fragment() {
         return inflater.inflate(R.layout.categories_fragment, container, false)
     }
 
-    companion object {
-    }
 }
