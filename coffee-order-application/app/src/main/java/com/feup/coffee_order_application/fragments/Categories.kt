c82fd9b1d56a2ed8531ebff65e1709eb6a3a05e2
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-20 18:23:58 +0000

    app/feat: added more categories

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/Categories.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/Categories.kt
@@ -12,11 +12,19 @@ import com.feup.coffee_order_application.R
 import com.feup.coffee_order_application.adapters.CategoriesAdapter
 import com.feup.coffee_order_application.models.Category
 
-
+val categories = listOf<Category>(
+    Category("Hot Coffee", R.drawable.hot_coffee, 10),
+    Category("Cold Coffee", R.drawable.ice_coffee, 12),
+    Category("Cappucino", R.drawable.cappucino, 8),
+    Category("Frappucino", R.drawable.frappuccino, 10),
+    Category("Mocha", R.drawable.mocha, 12),
+    Category("Oleato", R.drawable.oleato, 8)
+)
 class Categories : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,21 +32,17 @@ class Categories : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_categories, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (requireActivity() as AppCompatActivity).supportActionBar?.title = "Categories"
 
-        val categories = listOf<Category>(
-            Category("Hot Coffee", R.drawable.hot_coffee, 10),
-            Category("Cold Coffee", R.drawable.ice_coffee, 12),
-            Category("Cappucino", R.drawable.cappuccino, 8)
-        )
-
         val adapter = CategoriesAdapter(categories)
 
         val recyclerView: RecyclerView = view.findViewById(R.id.rv_categories)
-        recyclerView.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager =
+            GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
         recyclerView.adapter = adapter
     }
 
