f5b1da792e04ec2c6353203d681b0c252d523f73
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-26 12:13:58 +0000

    app/feat: added voucher adapter and voucher model

---
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/adapters/VoucherAdapter.kt
@@ -0,0 +1,52 @@
+package com.feup.coffee_order_application.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.feup.coffee_order_application.R
+import com.feup.coffee_order_application.databinding.VoucherCardBinding
+import com.feup.coffee_order_application.models.Voucher
+import kotlin.math.round
+
+class VoucherAdapter(private val vouchers: MutableList<Voucher>) :
+    RecyclerView.Adapter<VoucherAdapter.VoucherViewHolder>() {
+    private var quantityChangeListener: CartQuantityChangeListener? = null
+
+    class VoucherViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val voucherName: TextView = view.findViewById(R.id.tv_voucher_name)
+        val voucherCode: TextView = view.findViewById(R.id.tv_voucher_code)
+        val radioBtnSelected: TextView = view.findViewById(R.id.img_radio_btn_selected)
+        val radioBtn: TextView = view.findViewById(R.id.img_radio_btn)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VoucherViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.voucher_card, parent, false)
+        return VoucherViewHolder(view)
+    }
+
+    override fun getItemCount() = vouchers.size
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: VoucherViewHolder, position: Int) {
+        val voucher = vouchers[position]
+
+        holder.voucherName.text = if (voucher.type == "discount") "5 % OFF" else "Free Drink"
+        holder.voucherCode.text = voucher.uuid
+        holder.radioBtnSelected.visibility = if (voucher.isSelected) View.VISIBLE else View.GONE
+
+        holder.radioBtn.setOnClickListener {
+            for (v in vouchers) {
+                v.isSelected = false
+            }
+            voucher.isSelected = true
+            notifyDataSetChanged()
+        }
+
+    }
+
+
+}
