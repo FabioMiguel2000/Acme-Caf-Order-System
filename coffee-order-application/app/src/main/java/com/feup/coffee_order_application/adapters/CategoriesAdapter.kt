b414f9cf1633a9c6e2a394d97698959dee9aafe5
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-29 19:31:29 +0000

    app/feat: on click product category shows products

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/adapters/CategoriesAdapter.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/adapters/CategoriesAdapter.kt
@@ -1,13 +1,19 @@
 package com.feup.coffee_order_application.adapters
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.feup.coffee_order_application.R
+import com.feup.coffee_order_application.fragments.Products
 import com.feup.coffee_order_application.models.Category
 
 class CategoriesAdapter(private val categories: List<Category>) : RecyclerView.Adapter<CategoriesAdapter.CategoryViewHolder>() {
@@ -16,6 +22,7 @@ class CategoriesAdapter(private val categories: List<Category>) : RecyclerView.A
         val imageView: ImageView = view.findViewById(R.id.img_category)
         val nameTextView: TextView = view.findViewById(R.id.tv_category_name)
         val sizeTextView: TextView = view.findViewById(R.id.tv_category_size)
+        val layout: ConstraintLayout = view.findViewById(R.id.product_category_layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
@@ -29,6 +36,20 @@ class CategoriesAdapter(private val categories: List<Category>) : RecyclerView.A
         holder.nameTextView.text = category.name
         holder.sizeTextView.text = "(${category.size})"
         holder.imageView.setImageResource(category.imageSrc)
+
+
+        holder.layout.setOnClickListener {
+            val fragmentManager = (holder.layout.context as AppCompatActivity).supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val productsFragment = Products().apply {
+                arguments = Bundle().apply {
+                    putString("category", category.name)
+                }
+            }
+            fragmentTransaction.replace(R.id.fLayout, productsFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun getItemCount() = categories.size
--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/adapters/ProductsAdapter.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/adapters/ProductsAdapter.kt
@@ -9,17 +9,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.feup.coffee_order_application.R
 import com.feup.coffee_order_application.models.CartProduct
+import com.google.android.material.button.MaterialButton
+
 class ProductsAdapter(private val products: List<CartProduct>) : RecyclerView.Adapter<ProductsAdapter.ProductsViewHolder>() {
 
     class ProductsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val imageView: ImageView = view.findViewById(R.id.img_product)
         val nameTextView: TextView = view.findViewById(R.id.tv_product_name)
-        val btnAdd: ImageView = view.findViewById(R.id.btn_add_to_cart)
+        val btnAdd: MaterialButton = view.findViewById(R.id.btn_add_to_cart)
+        val priceTextView: TextView = view.findViewById(R.id.tv_product_price_per_piece)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.category_card, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_card, parent, false)
         return ProductsViewHolder(view)
     }
 
@@ -27,6 +30,7 @@ class ProductsAdapter(private val products: List<CartProduct>) : RecyclerView.Ad
     override fun onBindViewHolder(holder: ProductsViewHolder, position: Int) {
         val product = products[position]
         holder.nameTextView.text = product.name
+        holder.priceTextView.text = "${product.price} € / per piece"
         holder.imageView.setImageResource(product.imageUrl)
 
         holder.btnAdd.setOnClickListener {
