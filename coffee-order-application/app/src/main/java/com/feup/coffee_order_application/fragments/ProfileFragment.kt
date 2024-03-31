f524b60eadf42b93cd9247656efbbc148a9ece3e
Fab <fabiomiguelchenhuang@gmail.com>
2024-03-31 18:47:31 +0100

    app/feat: added request to get current user info for profile fragment

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/ProfileFragment.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/ProfileFragment.kt
@@ -1,15 +1,23 @@
 package com.feup.coffee_order_application.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.feup.coffee_order_application.R
+import com.feup.coffee_order_application.models.ApiResponse
+import com.feup.coffee_order_application.models.ResponseApi
+import com.feup.coffee_order_application.models.User
+import com.feup.coffee_order_application.services.ApiInterface
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProfileFragment : Fragment() {
-
+    private var user_id: String = "31ca6621550a71fdb4629390d1d264a2"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -26,5 +34,40 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         (requireActivity() as AppCompatActivity).supportActionBar?.title = "Hi User!"
+
+        fetchUserData()
+    }
+
+    private fun fetchUserData() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:3000/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiInterface::class.java)
+        val call = apiService.getUserById(user_id)
+
+        call.enqueue(object : retrofit2.Callback<ApiResponse<User>> {
+            override fun onResponse(call: retrofit2.Call<ApiResponse<User>>, response: retrofit2.Response<ApiResponse<User>>) {
+                if (response.isSuccessful) {
+                    val user = response.body()?.data
+                    Log.d("Response", response.body().toString())
+
+                    Log.d("User", user.toString())
+//                    user?.let {
+//                        updateUI(it)
+//                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponse<User>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun updateUI(user: User) {
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = "Hi ${user.name}!"
+        // Update other UI elements with user data as needed
     }
 }
--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/ApiInterface.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/ApiInterface.kt
@@ -1,12 +1,17 @@
 package com.feup.coffee_order_application.services
 
+import com.feup.coffee_order_application.models.ApiResponse
 import com.feup.coffee_order_application.models.ResponseApi
+import com.feup.coffee_order_application.models.User
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiInterface {
     @POST("auth/login")
     @JvmSuppressWildcards
     fun login(@Body body: Map<String, Any>): retrofit2.Call<ResponseApi>
+    @GET("users/{id}")
+    fun getUserById(@Path("id") userId: String): retrofit2.Call<ApiResponse<User>>
 }
