5edd30e3497f6fa6f7e17bd753456eb05c3b3809
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-03-31 00:51:03 +0000

    general function holder

---
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/General.kt
@@ -0,0 +1,14 @@
+package com.feup.coffee_order_application.services
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.util.Base64
+
+class General {
+
+    open fun baseImageToBitMap(image: String): Bitmap? {
+        val imageBytes = android.util.Base64.decode(image, android.util.Base64.DEFAULT)
+        return  BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+        //return decodedImage
+    }
+}
