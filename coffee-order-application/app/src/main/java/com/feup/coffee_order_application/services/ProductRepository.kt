33d2c96d456dc8f47f00660339370e4406f4e161
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-01 22:40:52 +0100

    app/feat: updated fetch product categories to handle new api response

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/ProductRepository.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/ProductRepository.kt
@@ -1,23 +1,29 @@
 package com.feup.coffee_order_application.services
 
 import android.util.Log
+import com.feup.coffee_order_application.models.ApiResponse
 import com.feup.coffee_order_application.models.Category
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ProductRepository(private val api: ApiInterface) {
     fun fetchProductCategories(callback: (List<Category>?) -> Unit) {
-        api.getProductCategories().enqueue(object : retrofit2.Callback<List<Category>> {
-            override fun onResponse(call: Call<List<Category>>, response: retrofit2.Response<List<Category>>) {
+        api.getProductCategories().enqueue(object : retrofit2.Callback<ApiResponse<List<Category>>> {
+            override fun onResponse(
+                call: Call<ApiResponse<List<Category>>>,
+                response: Response<ApiResponse<List<Category>>>
+            ) {
                 if (response.isSuccessful) {
-                    callback(response.body())
+                    callback(response.body()?.data)
                 } else {
                     Log.e("ProductRepo", "Failed to fetch product categories: ${response.message()}")
                     callback(null)
                 }
             }
-            override fun onFailure(call: Call<List<Category>>, t: Throwable) {
+
+            override fun onFailure(call: Call<ApiResponse<List<Category>>>, t: Throwable) {
                 Log.e("ProductRepo", "Error fetching product categories", t)
                 callback(null)
             }
