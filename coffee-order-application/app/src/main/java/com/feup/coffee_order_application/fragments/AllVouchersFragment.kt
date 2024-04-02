bf5b9bcd69d2e1ef3649e5e53941cf3ae80eabf4
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-03 00:19:31 +0100

    app/fix: issue resolved, fixed #57

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/AllVouchersFragment.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/AllVouchersFragment.kt
@@ -1,5 +1,6 @@
 package com.feup.coffee_order_application.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,8 +16,10 @@ import com.feup.coffee_order_application.services.ServiceLocator
 
 
 class AllVouchersFragment : Fragment() {
-    private var userId: String = "31ca6621550a71fdb4629390d1d264a2" // hardcoded user id, TODO: get from shared preferences (session)
+    private var userId: String = "ecf585f7874bc0d4c5f4f622dc93730b" // hardcoded user id, TODO: get from shared preferences (session)
     private val vouchers = mutableListOf<Voucher>()
+    private lateinit var unusedVoucherAdapter: AllVouchersAdapter
+    private lateinit var usedVoucherAdapter: AllVouchersAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,26 +30,38 @@ class AllVouchersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        unusedVoucherAdapter = AllVouchersAdapter(mutableListOf())
+        usedVoucherAdapter = AllVouchersAdapter(mutableListOf())
+
+        setupRecyclerView(view)
+
         fetchVouchers()
+
         setupActionBar()
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun fetchVouchers(){
-        ServiceLocator.userRepository.getUserVouchers(userId) { vouchers ->
-            vouchers?.let {
-                this.vouchers.addAll(it)
-                setupRecyclerView(requireView())
+        ServiceLocator.userRepository.getUserVouchers(userId) { fetchedVouchers ->
+            fetchedVouchers?.let {
+                vouchers.clear()
+                vouchers.addAll(it)
+
+                val usedVouchers = vouchers.filter { voucher -> voucher.used }.toMutableList()
+                val unusedVouchers = vouchers.filter { voucher -> !voucher.used }.toMutableList()
+
+                unusedVoucherAdapter.vouchers.clear()
+                unusedVoucherAdapter.vouchers.addAll(unusedVouchers)
+                unusedVoucherAdapter.notifyDataSetChanged()
+
+                usedVoucherAdapter.vouchers.clear()
+                usedVoucherAdapter.vouchers.addAll(usedVouchers)
+                usedVoucherAdapter.notifyDataSetChanged()
             }
         }
     }
 
     private fun setupRecyclerView(view: View) {
-        val usedVouchers = this.vouchers.filter { it.used }.toMutableList()
-        val unusedVoucher = this.vouchers.filter { !it.used }.toMutableList()
-
-        val unusedVoucherAdapter = AllVouchersAdapter(unusedVoucher)
-        val usedVoucherAdapter = AllVouchersAdapter(usedVouchers)
-
         view.findViewById<RecyclerView>(R.id.rv_used_vouchers).apply {
             layoutManager = LinearLayoutManager(context)
             this.adapter = usedVoucherAdapter
