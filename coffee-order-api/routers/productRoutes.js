49e7cd4303f2794c0d79b62acbecc19fe315864f
Sena <brunospereira1997@gmail.com>
2024-04-02 18:07:14 +0100

    Add fetch products by category

--- coffee-order-api/routers/productRoutes.js
+++ coffee-order-api/routers/productRoutes.js
@@ -1,10 +1,11 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
 router.get("/productCategories", productController.getProductCategories);
-router.get("/products", productController.getAllProducts);
-router.get("/products/:id", productController.getProductById);
 
+router.get("/product/category", productController.getProductByCategory);
+router.get("/products/:id", productController.getProductById);
+router.get("/products", productController.getAllProducts);
 
 module.exports = router;
