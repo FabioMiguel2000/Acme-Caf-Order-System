817243bbfc480a82bfd78aaf79d094d41b3efbba
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-03 22:29:15 +0100

    app/fix: fixed issue > app crashes when Go Shop Now btn is clicked #64

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/CategoriesFragment.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/fragments/CategoriesFragment.kt
@@ -47,11 +47,13 @@ class CategoriesFragment : Fragment() {
     }
 
     private fun updateRecyclerView() {
-        val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_categories)
-        (recyclerView.adapter as? CategoriesAdapter)?.let { adapter ->
-            adapter.categories.clear()
-            adapter.categories.addAll(this.categories)
-            adapter.notifyDataSetChanged()
+        if (isAdded) {
+            val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_categories)
+            (recyclerView.adapter as? CategoriesAdapter)?.let { adapter ->
+                adapter.categories.clear()
+                adapter.categories.addAll(this.categories)
+                adapter.notifyDataSetChanged()
+            }
         }
     }
 
