aa177cd4439158cb2bf36e7749df7fbaef524509
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-03 16:22:25 +0100

    app/feat: service locator getting base url from build config

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/ServiceLocator.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/services/ServiceLocator.kt
@@ -2,11 +2,11 @@ package com.feup.coffee_order_application.services
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.feup.coffee_order_application.BuildConfig
 
 object ServiceLocator {
-
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:3000/api/")
+        .baseUrl(BuildConfig.API_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
