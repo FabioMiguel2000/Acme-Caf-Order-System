05d49cd3fdffd8ba11636c8a1e08d4d4c48a7692
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-04 21:52:42 +0100

    app/feat: qr code size and background color

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/core/utils/QRCodeGenerator.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/core/utils/QRCodeGenerator.kt
@@ -1,19 +1,22 @@
 package com.feup.coffee_order_application.core.utils
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Color
+import com.feup.coffee_order_application.R
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
-class QRCodeGenerator {
-    fun generateQRCode(content: String, width: Int = 280, height: Int = 280): Bitmap? {
+class QRCodeGenerator() {
+    fun generateQRCode(content: String, width: Int = 320, height: Int = 320): Bitmap? {
         val qrCodeWriter = QRCodeWriter()
         try {
             val bitMatrix: BitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height)
-            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
             for (x in 0 until width) {
                 for (y in 0 until height) {
-                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) android.graphics.Color.BLACK else android.graphics.Color.WHITE)
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.TRANSPARENT)
                 }
             }
             return bitmap
