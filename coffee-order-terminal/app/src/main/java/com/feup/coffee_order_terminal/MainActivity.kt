624b0088ac5b8613eb42d9e570a28cae95a42065
Gabriel Gomes <gabrielsousagomes@hotmail.com>
2024-04-04 15:48:30 +0100

    :sparkles: - app/feat: Added Navbar terminal app

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/MainActivity.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/MainActivity.kt
@@ -3,18 +3,28 @@ package com.feup.coffee_order_terminal
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setTitle("Orders")
+        setSupportActionBar(toolbar)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->null
+                R.id.qrcode ->null
+            }
+            true
         }
     }
 }
