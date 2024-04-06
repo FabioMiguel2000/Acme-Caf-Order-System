d316a8d9606e6f2a52494536dd1268c9c175f3f7
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-06 18:36:51 +0100

    app/feat: added receipt fragment

--- coffee-order-application/app/src/main/java/com/feup/coffee_order_application/ui/adapter/ReceiptListAdapter.kt
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/ui/adapter/ReceiptListAdapter.kt
@@ -1,12 +1,17 @@
 package com.feup.coffee_order_application.ui.adapter
 
 import android.annotation.SuppressLint
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.feup.coffee_order_application.R
 import com.feup.coffee_order_application.core.utils.DateFormatter
 import com.feup.coffee_order_application.databinding.ReceiptCardBinding
 import com.feup.coffee_order_application.domain.model.Order
+import com.feup.coffee_order_application.ui.fragment.ProductsFragment
+import com.feup.coffee_order_application.ui.fragment.ReceiptFragment
 import kotlin.math.round
 
 class ReceiptListAdapter(
@@ -25,6 +30,15 @@ class ReceiptListAdapter(
         holder.binding.tvReceiptProductSize.text = "$totalProducts products"
         holder.binding.tvReceiptTotalPrice.text = "${round(receipt.total!! * 100) / 100} €"
         holder.binding.tvReceiptDate.text = DateFormatter().formatDate(receipt.date!!)
+
+        holder.binding.receiptCard.setOnClickListener {
+            val fragmentManager = (holder.itemView.context as AppCompatActivity).supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val receiptFragment = ReceiptFragment(receipt)
+            fragmentTransaction.replace(R.id.fLayout, receiptFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
     override fun getItemCount() = receipts.size
 
---
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/ui/fragment/ReceiptFragment.kt
@@ -0,0 +1,78 @@
+package com.feup.coffee_order_application.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.feup.coffee_order_application.R
+import com.feup.coffee_order_application.databinding.FragmentReceiptBinding
+import com.feup.coffee_order_application.domain.model.Order
+import com.feup.coffee_order_application.ui.adapter.CheckoutAdapter
+import kotlin.math.round
+
+class ReceiptFragment(val order: Order): Fragment() {
+    private var _binding: FragmentReceiptBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentReceiptBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        cartOrder = OrderStorageUtils.readOrderFromFile(requireContext())
+        setupActionBar()
+        setupRecyclerView()
+        updateUI()
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = CheckoutAdapter(order.products)
+
+        binding.rvOrder.apply {
+            layoutManager = LinearLayoutManager(context)
+            this.adapter = adapter
+        }
+    }
+
+    private fun updateUI() {
+//        binding.tvCheckoutCVoucherCode.text = order.freeCoffeeVoucher?._id?.toUpperCase()?: "No Voucher"
+//        binding.tvCheckoutDVoucherCode.text = order.discountVoucher?._id?.toUpperCase()?: "No Voucher"
+        updatePrices()
+    }
+
+
+    private fun setupActionBar() {
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            title = "Order Details"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    private fun updatePrices() {
+        val subtotalPrice = calculateSubtotalPrice()
+        val discountPrice = calculateDiscountPrice(subtotalPrice)
+        val totalPrice = calculateTotalPrice(subtotalPrice, discountPrice)
+
+        with(binding) {
+            tvSubtotalPrice.text = getString(R.string.price_format, subtotalPrice)
+            tvPromotionDiscount.text = getString(R.string.negative_price_format, discountPrice)
+            tvTotal.text = getString(R.string.price_format, totalPrice)
+        }
+    }
+
+    private fun calculateSubtotalPrice() = round(order.products.sumOf { it.product.price * it.quantity } * 100) / 100
+
+    private fun calculateDiscountPrice(subtotalPrice: Double) =
+        order.discountVoucher?.let { round(subtotalPrice * 0.05 * 100) / 100 } ?: 0.0
+
+    private fun calculateTotalPrice(subtotalPrice: Double, discountPrice: Double) = subtotalPrice - discountPrice
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
