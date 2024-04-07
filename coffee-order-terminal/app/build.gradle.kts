33f60d67ae4653db40841e46d653fa28969df78b
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-07 03:09:57 +0100

    retrofit lib definition

--- coffee-order-terminal/app/build.gradle.kts
+++ coffee-order-terminal/app/build.gradle.kts
@@ -54,5 +54,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Okhttp3 for the POST requests
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 
 }
