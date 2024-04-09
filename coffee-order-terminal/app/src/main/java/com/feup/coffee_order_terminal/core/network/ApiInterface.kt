bf39d9614518768f1707e67151682ec1a8064e6e
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-09 01:48:36 +0100

    defining method to validate order

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/core/network/ApiInterface.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/core/network/ApiInterface.kt
@@ -1,11 +1,15 @@
 package com.feup.coffee_order_terminal.core.network
 
 import com.feup.coffee_order_terminal.models.Order
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiInterface {
     @POST("orders/create")
     @JvmSuppressWildcards
     fun createOrder(@Body body: Map<String, Any?>): retrofit2.Call<ApiResponse<Order>>
+    @POST("orders/validation/{id}")
+    fun validateOrder(@Path("id") orderId: String): Call<ApiResponse<Order>>
 }
