d7927e1ea63bc629ac6382f402ad8690e10d253f
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-09 01:47:25 +0100

    start getting create order response

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/service/OrderApiCommunicator.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/service/OrderApiCommunicator.kt
@@ -1,8 +1,17 @@
 package com.feup.coffee_order_terminal.service
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.feup.coffee_order_terminal.OrderValidatorFragment
+import com.feup.coffee_order_terminal.R
 import com.feup.coffee_order_terminal.core.network.ApiResponse
 import com.feup.coffee_order_terminal.core.network.HttpHandlerClass
 import com.feup.coffee_order_terminal.models.CoffeeVoucher
@@ -31,9 +40,27 @@ class OrderApiCommunicator {
             ) {
                 if(response.code() == 201){
                     Toast.makeText(context, "Order created", Toast.LENGTH_LONG).show()
-                    val response = response.body();
-                    Log.e("response", response.toString())
+                    val response = response.body()?.data;
+                    val _id = response?._id
 
+                    //mostrar todas informações do pedido aqui
+                    val dialog = Dialog(context)
+                    dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    dialog.setContentView(R.layout.fragment_order_validator)
+                    dialog.setTitle("Order information:")
+                    var orderN = dialog.findViewById<TextView>(R.id.order_number)
+                    orderN.text = "Texte"
+                    val yesBtn = dialog.findViewById<Button>(R.id.btn_validate)
+                    yesBtn.setOnClickListener{
+                        //continuar aqui - validar pedido no servidor
+                        dialog.dismiss()
+                    }
+
+                    val notBtn = dialog.findViewById<Button>(R.id.cancel)
+                    notBtn.setOnClickListener{
+                        dialog.dismiss()
+                    }
+                    dialog.show()
                 } else {
                   Toast.makeText(context, "Something went wrong, please try again later", Toast.LENGTH_LONG).show()
                 }
