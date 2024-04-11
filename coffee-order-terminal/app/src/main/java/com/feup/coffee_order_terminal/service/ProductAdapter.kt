f8216a6ae68f205320389f7202e3d44f3dbc038b
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-11 01:35:44 +0100

    order detail well positioned

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/service/ProductAdapter.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/service/ProductAdapter.kt
@@ -14,8 +14,8 @@ import com.feup.coffee_order_terminal.models.ProductOrder
 class ProductAdapter (val products: MutableList<ProductOrder>): RecyclerView.Adapter<ProductAdapter.ProductViewHolder>(){
     class ProductViewHolder(view: View): RecyclerView.ViewHolder(view){
         val pNome: TextView = view.findViewById(R.id.prod_name)
-            //val pCategory: TextView = view.findViewById(R.id.prod_category)
-        //val pQtd: TextView = view.findViewById(R.id.prod_qtd)
+        val pCategory: TextView = view.findViewById(R.id.prod_category)
+        val pQtd: TextView = view.findViewById(R.id.prod_qtd)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
@@ -30,7 +30,7 @@ class ProductAdapter (val products: MutableList<ProductOrder>): RecyclerView.Ada
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val product = products[position]
         holder.pNome.text = product.product.name
-        //holder.pCategory.text = product.category.name
-        //holder.pQtd.text = "3"
+        holder.pCategory.text= product.product.category.name
+        holder.pQtd.text= product.quantity
     }
 }
