7dad3ce8838ab166f921d5294801941392e60334
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-14 00:59:40 +0100

    get order with order id from fragment parameter

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/OrderFragment.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/OrderFragment.kt
@@ -16,9 +16,8 @@ import com.feup.coffee_order_terminal.service.OrderApiCommunicator
 import com.feup.coffee_order_terminal.ui.adapter.ProductAdapter
 
 class OrderFragment : Fragment() {
-    var orderId = this.arguments?.getString("order")
+    var orderId = ""
     private val products = mutableListOf<ProductOrder>()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +34,7 @@ class OrderFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView(view)
+        this.orderId = this.arguments?.getString("order").toString()
         getOrder()
 
         val btnValidate : Button = requireView().findViewById(R.id.btn_validate_order)
@@ -50,14 +50,15 @@ class OrderFragment : Fragment() {
 
     private fun getOrder(){
         val orderApiCommunicator = OrderApiCommunicator()
-        orderApiCommunicator.getOrder( requireContext(), "752d04417455bbffa4af0b7b9fa58750" ){
-            order -> order.let {
+        this.orderId?.let {
+            orderApiCommunicator.getOrder( requireContext(), it){
+                order -> order.let {
                 products.clear()
                 products.addAll(it!!.products)
                 updateRecyclerView()
             }
-
-            changeOrderInformation(order!!.client.nif, order.client.email, order.client.name, order._id, order.date, order.total, order.subtotal)
+                changeOrderInformation(order!!.client.nif, order.client.email, order.client.name, order._id, order.date, order.total, order.subtotal)
+            }
         }
     }
 
@@ -71,10 +72,8 @@ class OrderFragment : Fragment() {
     private fun updateRecyclerView() {
         if(isAdded){
             val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_product_list)
-            Log.e("productUpdate", this.products.toString())
             (recyclerView.adapter as? ProductAdapter)?.let { adapter ->
                 adapter.products.clear()
-                Log.e("productUpdate", this.products.toString())
                 adapter.products.addAll(products)
                 adapter.notifyDataSetChanged() // Notify the adapter of the data change - there's a pub sub inside
             }
@@ -89,7 +88,7 @@ class OrderFragment : Fragment() {
         val orderDate: TextView = requireView().findViewById(R.id.order_date)
         val orderTotal: TextView = requireView().findViewById(R.id.order_total_price)
         val orderSubTotal: TextView = requireView().findViewById(R.id.order_subtotal_price)
-        val totalTV: TextView = requireView().findViewById(R.id.tv_total)
+        //val orderStatus: TextView = requireView().findViewById(R.id.)
         orderUserEmail.text = email
         orderUser.text = nif
         orderUserName.text = name
