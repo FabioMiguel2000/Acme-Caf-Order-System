1272c49f259fea778ae4f2aba0ee47510f30cd59
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-14 21:41:46 +0100

    terminal/feat: added Order List Adapter

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/activity/MainActivity.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import com.feup.coffee_order_terminal.ui.fragment.QRCodeFragment
 import com.feup.coffee_order_terminal.R
 import com.feup.coffee_order_terminal.ui.fragment.OrderFragment
+import com.feup.coffee_order_terminal.ui.fragment.OrderListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val orderList = OrderFragment()
+        val orderList = OrderListFragment()
         val qrcode = QRCodeFragment()
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
 
--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/fragment/OrderListFragment.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/fragment/OrderListFragment.kt
@@ -1,6 +1,7 @@
 package com.feup.coffee_order_terminal.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.feup.coffee_order_terminal.R
 import com.feup.coffee_order_terminal.core.service.ServiceLocator
 import com.feup.coffee_order_terminal.domain.model.Order
+import com.feup.coffee_order_terminal.ui.adapter.OrderListAdapter
 import com.feup.coffee_order_terminal.ui.adapter.OrdersAdapter
 
-class ReceiptListFragment : Fragment() {
-    private val orders = mutableListOf<Order>()
-//    private lateinit var receiptListAdapter: ReceiptListAdapter
+class OrderListFragment : Fragment() {
+//    private val orders = mutableListOf<Order>()
+    private lateinit var orderListAdapter: OrderListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,42 +29,45 @@ class ReceiptListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        orderListAdapter = OrderListAdapter(mutableListOf())
+
         setupRecyclerView(view)
+
+        setupActionBar()
 
         ServiceLocator.orderRepository.getOrders { orders ->
             orders?.let {
-                this.orders.clear()
-                this.orders.addAll(it)
-                updateRecyclerView()
+                orderListAdapter.orders.clear()
+                orderListAdapter.orders.addAll(it)
+                orderListAdapter.notifyDataSetChanged()
             }
         }
     }
 
-
     private fun setupRecyclerView(view: View) {
         view.findViewById<RecyclerView>(R.id.rv_orders).apply {
             layoutManager = LinearLayoutManager(context)
-//            this.adapter = receiptListAdapter
+            this.adapter = orderListAdapter
         }
     }
 
     private fun setupActionBar() {
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
-            title = "Receipts"
-            setDisplayHomeAsUpEnabled(true)
-            setDisplayShowHomeEnabled(true)
+            title = "Orders"
+            setDisplayHomeAsUpEnabled(false)
+            setDisplayShowHomeEnabled(false)
         }
     }
 
-    private fun updateRecyclerView() {
-        if (isAdded) {
-            val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_orders)
-            (recyclerView.adapter as? OrdersAdapter)?.let { adapter ->
-                adapter.orders.clear()
-                adapter.orders.addAll(this.orders)
-                adapter.notifyDataSetChanged()
-            }
-        }
-    }
+//    private fun updateRecyclerView() {
+//        if (isAdded) {
+//            val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_orders)
+//            (recyclerView.adapter as? OrdersAdapter)?.let { adapter ->
+//                adapter.orders.clear()
+//                adapter.orders.addAll(this.orders)
+//                adapter.notifyDataSetChanged()
+//            }
+//        }
+//    }
 
 }
