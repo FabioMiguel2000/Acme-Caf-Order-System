7f7054897778b449312f565513c800000314f131
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-14 20:48:31 +0100

    terminal/feat: added arrow icon

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/fragment/OrderListFragment.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/fragment/OrderListFragment.kt
@@ -24,6 +24,9 @@ class OrderListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
