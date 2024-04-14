b432889058866847c5cc79e6b1b2ef46451a6476
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-14 22:29:25 +0100

    terminal/feat: enabled go back fragment

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/activity/MainActivity.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.feup.coffee_order_terminal.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -40,6 +42,13 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
     }
-
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
