fd011882c2ac08997eda3264fb85e1157fa1b58c
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-16 14:38:26 +0100

    terminal/feat: added success fragment layout

--- coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/fragment/OrderConfirmationFragment.kt
+++ coffee-order-terminal/app/src/main/java/com/feup/coffee_order_terminal/ui/fragment/OrderConfirmationFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.feup.coffee_order_terminal.R
@@ -19,10 +20,6 @@ import com.feup.coffee_order_terminal.ui.adapter.ProductsAdapter
 class OrderConfirmationFragment : Fragment() {
     var orderId = ""
     private val products = mutableListOf<CartProduct>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,43 +35,62 @@ class OrderConfirmationFragment : Fragment() {
         this.orderId = this.arguments?.getString("order").toString()
         getOrder()
 
-        val btnValidate : Button = requireView().findViewById(R.id.btn_validate_order)
+        val btnValidate: Button = requireView().findViewById(R.id.btn_validate_order)
         btnValidate.setOnClickListener {
             val orderApiCommunicator = OrderApiCommunicator()
             this.orderId?.let { it1 ->
-                orderApiCommunicator.validateOrder(this.requireContext(),
+                orderApiCommunicator.validateOrder(
+                    this.requireContext(),
                     it1
                 )
+                // Navigate to the OrderListFragment
+                val fragment = OrderListFragment()
+                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fLayout, fragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+
             }
         }
     }
 
-    private fun getOrder(){
+    private fun getOrder() {
         val orderApiCommunicator = OrderApiCommunicator()
         this.orderId.let {
-            orderApiCommunicator.getOrder( requireContext(), it){
+            orderApiCommunicator.getOrder(requireContext(), it) {
 
-                    order -> order.let {
-                products.clear()
-                products.addAll(it!!.products)
-                updateRecyclerView()
-            }
-                changeOrderInformation(order!!.client!!.nif, order.client!!.email, order.client!!.name, order._id!!, order.date!!, order.total.toString(), order.subtotal.toString(), order.promotionDiscount.toString(), order?.freeCoffeeVoucher?._id,
+                order ->
+                order.let {
+                    products.clear()
+                    products.addAll(it!!.products)
+                    updateRecyclerView()
+                }
+                changeOrderInformation(
+                    order!!.client!!.nif,
+                    order.client!!.email,
+                    order.client!!.name,
+                    order._id!!,
+                    order.date!!,
+                    order.total.toString(),
+                    order.subtotal.toString(),
+                    order.promotionDiscount.toString(),
+                    order?.freeCoffeeVoucher?._id,
                     order?.discountVoucher?._id
                 )
             }
         }
     }
 
-    private fun setupRecyclerView(view: View){
+    private fun setupRecyclerView(view: View) {
         val adapter = ProductsAdapter(mutableListOf())
         view.findViewById<RecyclerView>(R.id.rv_product_list).apply {
             layoutManager = LinearLayoutManager(context)
             this.adapter = adapter
         }
     }
+
     private fun updateRecyclerView() {
-        if(isAdded){
+        if (isAdded) {
             val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_product_list)
             (recyclerView.adapter as? ProductsAdapter)?.let { adapter ->
                 adapter.products.clear()
@@ -85,7 +101,18 @@ class OrderConfirmationFragment : Fragment() {
     }
 
     @SuppressLint("SetTextI18n")
-    private fun changeOrderInformation(nif: String, email: String, name: String, orderId: String, date: String, total: String, subTotal: String, orderPromotionDiscount: String, freeCoffeeVoucher: String?, discountVoucher: String?){
+    private fun changeOrderInformation(
+        nif: String,
+        email: String,
+        name: String,
+        orderId: String,
+        date: String,
+        total: String,
+        subTotal: String,
+        orderPromotionDiscount: String,
+        freeCoffeeVoucher: String?,
+        discountVoucher: String?
+    ) {
         val orderUser: TextView = requireView().findViewById(R.id.order_owner)
         val orderUserEmail: TextView = requireView().findViewById(R.id.order_owner_email)
         val orderUserName: TextView = requireView().findViewById(R.id.order_owner_name)
@@ -95,7 +122,8 @@ class OrderConfirmationFragment : Fragment() {
         val orderSubTotal: TextView = requireView().findViewById(R.id.order_subtotal_price)
         val promotionDiscount: TextView = requireView().findViewById(R.id.order_promotion_discount)
         val freeCoffe: TextView = requireView().findViewById(R.id.order_coffe_voucher_code)
-        val discountVoucherV :TextView = requireView().findViewById(R.id.order_discount_voucher_code)
+        val discountVoucherV: TextView =
+            requireView().findViewById(R.id.order_discount_voucher_code)
         orderUserEmail.text = "Email: $email"
         orderUser.text = "VAT Number: $nif"
         orderUserName.text = "Name: $name"
@@ -104,8 +132,8 @@ class OrderConfirmationFragment : Fragment() {
         orderTotal.text = total
         orderSubTotal.text = subTotal
         promotionDiscount.text = orderPromotionDiscount
-        freeCoffe.text = freeCoffeeVoucher?.uppercase()?: "No Voucher"
-        discountVoucherV.text = discountVoucher?.uppercase()?: "No Voucher"
+        freeCoffe.text = freeCoffeeVoucher?.uppercase() ?: "No Voucher"
+        discountVoucherV.text = discountVoucher?.uppercase() ?: "No Voucher"
         this.orderId = orderId
     }
 }
