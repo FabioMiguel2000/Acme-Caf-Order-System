ac7c21af141486aa17709bcfe2e32eab0d8d15ec
Fab <fabiomiguelchenhuang@gmail.com>
2024-04-16 17:58:46 +0100

    app/feat: added success fragment

---
+++ coffee-order-application/app/src/main/java/com/feup/coffee_order_application/ui/fragment/SuccessFragment.kt
@@ -0,0 +1,38 @@
+package com.feup.coffee_order_application.ui.fragment
+
+import com.feup.coffee_order_application.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+
+class SuccessFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_success, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupActionBar()
+        view.findViewById<MaterialButton>(R.id.btn_go_back).setOnClickListener {
+            val fragment = HomeFragment()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fLayout, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+    }
+
+    private fun setupActionBar() {
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            title = "Success"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+}
